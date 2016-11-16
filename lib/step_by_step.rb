981627555c1fcce3f99d26a276f49bf664546f1d
Konrad Jurkowski <konrad@jurkowski.me>
2016-11-16 13:52:14 +0100

    Used single quotes instead of double quotes

--- lib/step_by_step.rb
+++ lib/step_by_step.rb
@@ -1,8 +1,8 @@
-require "active_record"
-require "rails"
-require "step_by_step/version"
-require "step_by_step/rollout"
-require "step_by_step/controller"
+require 'active_record'
+require 'rails'
+require 'step_by_step/version'
+require 'step_by_step/rollout'
+require 'step_by_step/controller'
 
 module StepByStep
   class Engine < ::Rails::Engine
